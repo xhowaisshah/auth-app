9f785e9810f26cb1df1a74dcf7f74b10180226a6
Xh Owais <87980770+XhOwais@users.noreply.github.com>
2024-02-06 16:03:47 +0500

    validation updated...

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,13 @@ export const LoginSchema = z.object({
         .string().min(1, { message: 'Password is required' }),
 })
 
+export const RegisterSchema = z.object({
+    name: z.string().min(1, { message: 'Name is required' }),
+    email: z
+        .string()
+        .min(1, { message: 'Email is required' })
+        .email({ message: 'Email is invalid' }),
+    password: z
+        .string().min(6, { message: 'Minimum 6 characters required' }),
+})
+
